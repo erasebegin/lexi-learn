3a70ff60f038c155ff3e7f5f8f8c1cd71b463979
chris-haupt <chris@apacio.com>
2021-08-16 07:33:06 +0100

    removed keys

--- src/firebase.js
+++ src/firebase.js
@@ -2,11 +2,8 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-console.log(process.env.REACT_APP_FIREBASE_API_KEY);
-console.log('AIzaSyAK-euVdEeXJ-DZRJby0pG7QktCOqMP6Ks');
-
 const app = initializeApp({
-  apiKey: 'AIzaSyAK-euVdEeXJ-DZRJby0pG7QktCOqMP6Ks',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
